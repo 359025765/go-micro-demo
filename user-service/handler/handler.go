f181dde485b72f60efef381753a3ffb022f0c234
chenjiaxing <star359025765@gmail.com>
2020-10-09 14:19:11 +0800

    完成用户服务中的handler(controller)和main

---
+++ user-service/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	pb "github.com/359025765/go-micro-demo/user-service/proto/user"
+	"github.com/359025765/go-micro-demo/user-service/repo"
+	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/net/context"
+)
+
+type UserService struct {
+	Repo repo.Repository
+}
+
+func (srv *UserService) Get(ctx context.Context, req *pb.User, res *pb.Response) error {
+	user, err := srv.Repo.Get(req.Id)
+	if err != nil {
+		return err
+	}
+	res.User = user
+	return nil
+}
+
+func (srv *UserService) GetAll(ctx context.Context, req *pb.Request, res *pb.Response) error {
+	users, err := srv.Repo.GetAll()
+	if err != nil {
+		return err
+	}
+	res.Users = users
+	return nil
+}
+
+//func (srv *UserService) GetByEmail (ctx context.Context, req *pb.Request, res *pb.Response) error {
+//    users, err := srv.Repo.GetByEmail(req.Email)
+//}
+
+func (srv *UserService) Create(ctx context.Context, req *pb.User, res *pb.Response) error {
+	hashedPwd, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	req.Password = string(hashedPwd)
+	if err := srv.Repo.Create(req); err != nil {
+		return err
+	}
+	res.User = req
+	return nil
+}
---
+++ user-service/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	database "github.com/359025765/go-micro-demo/user-service/db"
+	"github.com/359025765/go-micro-demo/user-service/handler"
+	pb "github.com/359025765/go-micro-demo/user-service/proto/user"
+	repositroy "github.com/359025765/go-micro-demo/user-service/repo"
+	"github.com/micro/go-micro"
+	"log"
+)
+
+func main() {
+
+	// 创建数据库连接，程序退出时断开连接
+	db, err := database.CreateConnection()
+	defer db.Close()
+
+	if err != nil {
+		log.Fatalf("Could not connect to DB: %v", err)
+	}
+
+	// 每次启动服务都会检查，如果数据表不存在则创建，已存在检查是否有修改
+	db.AutoMigrate(&pb.User{})
+
+	// 初始化Repo实例用于后续数据库操作
+	repo := &repositroy.UserRepository{db}
+
+	// 创建微服务
+	service := micro.NewService(
+		micro.Name("laracom.user.service"),
+		micro.Version("latest"),
+	)
+	service.Init()
+
+	// 注册处理器
+	pb.RegisterUserServiceHandler(service.Server(), &handler.UserService{repo})
+
+	// 启动用户服务
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
