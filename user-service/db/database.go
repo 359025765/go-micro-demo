c5f5dafa9aedd6423efb8ef7c25745ee3058149c
chenjiaxing <star359025765@gmail.com>
2020-10-12 16:14:29 +0800

    取掉user表的 deleted_at 字段

--- user-service/db/database.go
+++ user-service/db/database.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"os"
+	//"os"
 )
 
 func CreateConnection() (*gorm.DB, error) {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	DBName := os.Getenv("DB_NAME")
-	password := os.Getenv("DB_PASSWORD")
+	//host := os.Getenv("DB_HOST")
+	//user := os.Getenv("DB_USER")
+	//DBName := os.Getenv("DB_NAME")
+	//password := os.Getenv("DB_PASSWORD")
+	host := "127.0.0.1:3308"
+	user := "root"
+	DBName := "micro-homestead"
+	password := "secret"
 
 	return gorm.Open(
 		"mysql",
